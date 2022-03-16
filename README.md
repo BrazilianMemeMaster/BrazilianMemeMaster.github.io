9ed8bf4b5a89655fab4a8173fe0ade47e8bfa1b0
BrazilianMemeMaster <aalexandrexd@hotmail.com>
2022-03-16 16:12:06 -0300

    update using axios

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# BrazilianMemeMaster.github.io
+# Frontend Mentor - Advice generator app solution
+
+This is a solution to the [Advice generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Generate new advice everytime they click the dice;
+
+### Screenshot
+
+![](./images/screenshot.jpg)
+
+### Links
+
+- Live Site URL: [](https://brazilianmememaster.github.io/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [Axios](https://github.com/axios/axios) - JS library
+
+### What I learned
+
+- How to send a request using fetch or axios properly.
+
+## Author
+
+- Frontend Mentor - [@BrazilianMemeMaster](https://www.frontendmentor.io/profile/brazilianmememaster)
+- Twitter - [@MemeMaster_xd](https://www.twitter.com/mememaster_xd)
--- app.js
+++ app.js
@@ -2,20 +2,30 @@ const btn = document.querySelector('#dice');
 const advice = document.querySelector('#advice');
 const adviceNum = document.querySelector('#number');
 
-function fetchAdvice() {
-    try {
-        fetch('https://api.adviceslip.com/advice')
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                advice.innerText = `\u201C${data.slip.advice}\u201D`;
-                adviceNum.innerText = data.slip.id;
-            })
-    } catch (e) {
-        console.log("Something Went Wrong!", e)
-    }
+// function fetchAdvice() {
+//     try {
+//         fetch('https://api.adviceslip.com/advice')
+//             .then(res => {
+//                 return res.json()
+//             })
+//             .then(data => {
+//                 advice.innerText = `\u201C${data.slip.advice}\u201D`;
+//                 adviceNum.innerText = data.slip.id;
+//             })
+//     } catch (e) {
+//         console.log("Something Went Wrong!", e)
+//     }
+// }
+
+// fetchAdvice();
+
+const axiosAdvice = async() => {
+    const res = await axios.get('https://api.adviceslip.com/advice');
+    console.log(res.data)
+    advice.innerText = `\u201C${res.data.slip.advice}\u201D`;
+    adviceNum.innerText = res.data.slip.id;
+
 }
 
-fetchAdvice();
-btn.addEventListener('click', fetchAdvice)
+axiosAdvice();
+btn.addEventListener('click', axiosAdvice)
